b717bae1f52b5f483b4f8ccc2b4287589cca65df
Olexii <nagvval@gmail.com>
2024-03-28 15:53:31 +0200

    GUI project on Kivy

---
+++ Version03/main.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+from kivy.config import Config
+from kivy.uix.button import Button
+
+Config.set('graphics', 'borderless', '1')
+Config.set('graphics', 'fullscreen', 'auto')
+
+class MyApp(App):
+    def build(self):
+        return Button(text = "Кнопка",
+                      font_size = 30,
+                      background_color = [0, 1, 0, 1],
+                      border = (20, 20, 20, 20)
+                       )
+
+if __name__ == "__main__":
+    MyApp().run()
+    
+    
